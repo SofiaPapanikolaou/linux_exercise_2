2c53e2b02ada8bf58b689330ada1c4bed0a910d6
Sofia Papanikolaou <papanikolaou.sofia@hotmail.com>
2018-12-02 16:13:09 +0200

    pattern.sh

--- pattern.sh
+++ pattern.sh
@@ -8,3 +8,8 @@ cat fasta.fa.2 | grep -ie '[ACGT]_[ACGT]_[ACGT]' | wc -l
 cat fasta.fa.2 | grep -v '^>' | grep -v '-' | wc -l
 2452
 cat fasta.fa.2 | grep -ie 'GGG[CT][AG]'
+
+# to export only the sequence names and find out how many sequences are from human (hg19)
+cat fasta.fa.2 | grep -e '^>'
+cat fasta.fa.2 | grep -e '^>' | grep -ie 'hg19' | wc -l
+50
